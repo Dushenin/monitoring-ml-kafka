d5bf4aad2b5cbe63093560d751b1327f501811c7
Kolesnikov Dmitry <koldim2001@yandex.ru>
2024-03-29 21:24:26 +0300

    docker change'

--- src/make_folders.bat
+++ src/make_folders.bat
@@ -1,11 +1,2 @@
-#!/bin/bash
-
-# Создаем необходимые папки
-mkdir -p ./ml_monitoring_services/pg_ml
-mkdir -p ./ml_monitoring_services/pg_grafana 
-mkdir -p ./ml_monitoring_services/grafana
-mkdir -p ./ml_monitoring_services/s3_storage
-mkdir -p ./ml_monitoring_services/prometheus
-
 # Запускаем docker-compose
 docker-compose -p ml_services_diploma up -d
